80ae0844faadc5701c2af786c6a0e79548763078
Luka <luanafernandes73@gmail.com>
2020-08-28 13:39:04 -0400

    Add entities and main for testing

---
+++ Maxime/Entities/Registradora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxime.Entities
+{
+    public class Registradora
+    {
+        private double total;
+
+        public void addProduto(Produto produto)
+        {
+            total = total + produto.Valor + produto.ImpostoFederal;
+        }
+
+        public double getTotal()
+        {
+            return total;
+        }
+
+        public string getNomeDoProduto(Produto produto)
+        {
+            //código/modelo – marca
+            return produto.Codigo + "/" + produto.Modelo + " - " + produto.Marca;
+        }
+    }
+}
